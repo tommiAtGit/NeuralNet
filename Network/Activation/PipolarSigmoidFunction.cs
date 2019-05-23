77f2cd56a08e4df35fe786408a0bfc897a17b534
Tommi Korteikko <tommi.korteikko@midare.fi>
2019-05-23 23:45:24 +0300

    Activation functions added

--- Network/Activation/PipolarSigmoidFunction.cs
+++ Network/Activation/PipolarSigmoidFunction.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace NeuralNetwork.Activation
 {
-    class PipolarSigmoidFunction:IActivationFunction
+    class PipolarSigmoidFunction : IActivationFunction
     {
+        private double _coeficient;
+
+        public PipolarSigmoidFunction(double coeficient)
+        {
+            _coeficient = coeficient;
+        }
+        public double CalculateOutput(double input)
+        {
+            return (1 / (1 + Math.Exp(-input * _coeficient)));
+        }
     }
 }
--- Network/Activation/SigmoidFunction.cs
+++ Network/Activation/SigmoidFunction.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace NeuralNetwork.Activation
 {
-    class SigmoidFunction:IActivationFunction
+    class SigmoidFunction : IActivationFunction
     {
+       
+
+        public SigmoidFunction(double coeficient)
+        {
+            
+        }
+        double IActivationFunction.CalculateOutput(double input)
+        {
+            return 1 / (1 + Math.Exp(-input));
+        }
     }
 }
